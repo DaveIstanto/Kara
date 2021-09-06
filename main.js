34340a30e81b1178cbeddaab7028ae964219ab90
Dave Istanto <daveistanto@Daves-Machine.local>
2021-09-06 01:11:22 -0700

    Use command separately

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
-const { Client, Intents } = require('discord.js')
+const fs = require('fs')
+const { Client, Collection, Intents } = require('discord.js')
+const { token } = require('./config.json')
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 })
 
+// Setup commands collection, get all .js file from ./commands directory
+client.commands = new Collection()
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'))
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command)
+}
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+  console.log(`Bot logged in as ${client.user.tag}!`)
 })
 
-// Prefix is !
-const prefix = '!'
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return
 
-client.on('message', (msg) => {
-  // Do nothing if prefix is not !
-  if (!msg.content.startsWith(prefix) || msg.author.bot) {
-    return
+  const { commandName } = interaction
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!')
+  } else if (commandName === 'server') {
+    await interaction.reply('Server info.')
+  } else if (commandName === 'user') {
+    await interaction.reply('User info.')
   }
-
-  // Parse message
 })
 
-const TOKEN = require('./token')
-
-client.login(TOKEN)
+client.login(token)
